b41083cec9524c5ed115ed32d9dc153af98ffb67
Daouadji Aymen <daouadji506@gmail.com>
2023-03-21 22:21:05 +0100

    ➕ add assets

--- src/renderer/assets/styles/color.ts
+++ src/renderer/assets/styles/color.ts
@@ -1,48 +1,14 @@
-const nighty = {
-  cold_red: '#ff4b59',
-  background: '#23315e',
-  lighter_background: '#2c3964',
-  cold_blue: '#01b9ca',
-  hot_red: '#f44336',
-  good_green: '#0acf83',
-  secondary_color: '#344175',
-  darkersec_color: '#29336a',
-  border_color: '#3c4b7e',
-  hot_purple: '#626ED4',
-  silver_gray: '#646d8d',
-  white: '#fff',
-  text_gray: '#bdbdbd',
-  good_black: '#18191c',
-  warm_orange: '#f2994a',
-  darker: ' #00000020',
-  light: '#ffffff20',
-  coldBlack: '#374151',
-};
-
-const drakula = {
-  cold_red: '#C5434D',
-  background: '#303030',
-  lighter_background: '#404040',
-  cold_blue: '#439297',
-  hot_red: '#C13E34',
-  good_green: '#1D7D58',
-  secondary_color: '#363636',
-  darkersec_color: '#262626',
-  border_color: '#ffffff0d',
-  hot_purple: '#4D8F87',
-  silver_gray: '#5a6a67',
-  white: '#ffffff',
-  text_gray: '#bdbdbd',
-  good_black: '#18191c',
-  warm_orange: '#f2994a',
-  darker: '#00000020',
-  light: '#ffffff20',
-};
-const staticColors = nighty;
-
 const color = {
-  cold_red: 'var(--cold-red)',
   background: 'var(--background)',
+  lighten: 'var(--lighten)',
+  textColor: 'var(--text-color)',
+  darken: 'var(--darken)',
+  darkColor: 'var(--dark-color)',
+  textGray: 'var(--text-gray)',
+  red: 'var(--red)',
+  primary: 'var(--primary)',
+  cold_red: 'var(--cold-red)',
+
   lighter_background: 'var(--lighter-background)',
   cold_blue: 'var(--cold-blue)',
   hot_red: 'var(--hot-red)',
@@ -60,5 +26,6 @@ const color = {
   light: 'var(--light)',
   coldBlack: 'var(--cold-black)',
 };
+
 export default color;
-export { color, staticColors };
+export { color };
