3014fd3e8d5adace44c3eb99aedb03972da256f3
Daouadji Aymen <daouadji506@gmail.com>
2023-03-23 23:27:37 +0100

    💄 create ImageGallery

---
+++ src/renderer/ui/components/image_gallery/ImageGallery.tsx
@@ -0,0 +1,75 @@
+import ColorfulButton from '@components/buttons/colorful_button';
+import './style/index.scss';
+import profile from '@assets/pictures/example.png';
+import ImageItem from '@components/image_item';
+
+const imageArray = [
+  {
+    url: profile,
+    alt: 'profile',
+
+    name: 'harnan',
+    price: 500,
+  },
+  {
+    url: profile,
+    alt: 'profile',
+
+    name: 'harnan',
+    price: 500,
+  },
+  {
+    url: profile,
+    alt: 'profile',
+
+    name: 'harnan',
+    price: 500,
+  },
+  {
+    url: profile,
+    alt: 'profile',
+
+    name: 'harnan',
+    price: 500,
+  },
+  {
+    url: profile,
+    alt: 'profile',
+
+    name: 'harnan',
+    price: 500,
+  },
+  {
+    url: profile,
+    alt: 'profile',
+
+    name: 'harnan',
+    price: 500,
+  },
+  {
+    url: profile,
+    alt: 'profile',
+
+    name: 'harnan',
+    price: 500,
+  },
+];
+
+interface ImageGalleryProps {}
+export default function ImageGallery({}: ImageGalleryProps) {
+  return (
+    <div className="image-gallery">
+      <ColorfulButton
+        text="View all art"
+        alignSelf="center"
+        width={'70%'}
+        padding={10}
+      />
+      <div className="images-container">
+        {imageArray.map((image, index) => (
+          <ImageItem key={index} {...image} />
+        ))}
+      </div>
+    </div>
+  );
+}
