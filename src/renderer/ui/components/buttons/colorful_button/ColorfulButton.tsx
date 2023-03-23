0334cc6d50d64e4bce99546a85a314a234290dd7
Daouadji Aymen <daouadji506@gmail.com>
2023-03-23 20:15:06 +0100

    ✅ update ColorfulButton

--- src/renderer/ui/components/buttons/colorful_button/ColorfulButton.tsx
+++ src/renderer/ui/components/buttons/colorful_button/ColorfulButton.tsx
@@ -1,6 +1,5 @@
-import color from '@assets/styles/color';
+import { color } from '@assets/styles/color';
 import TextButton from '../text_button';
-import './style/index.scss';
 import { ComponentProps } from 'react';
 interface ColorfulButtonProps {
   isActive?: boolean;
@@ -21,21 +20,18 @@ export default function ColorfulButton({
     | 'radius'
   >) {
   return (
-    <div className="colorful-border">
-      <TextButton
-        fontColor={fontColor}
-        fontSize={14}
-        backgroundColor={
-          'linear-gradient(rgb(41, 43, 51), rgb(41, 43, 51)) no-repeat padding-box, linear-gradient(81.02deg, rgb(250, 85, 96) -23.47%, rgb(177, 75, 244) 45.52%, rgb(77, 145, 255) 114.8%) border-box'
-        }
-        afterBgColor={undefined}
-        // borderColor={!isActive ? color.primary : undefined}
-        radius={9}
-        css={{
-          backgroundImage: isActive && !blend ? color.primary : undefined,
-        }}
-        {...others}
-      />
-    </div>
+    <TextButton
+      fontColor={fontColor}
+      fontSize={14}
+      backgroundColor={`${
+        isActive && !blend
+          ? color.primary
+          : 'linear-gradient(rgb(41, 43, 51), rgb(41, 43, 51))'
+      }   no-repeat padding-box, linear-gradient(81.02deg, rgb(250, 85, 96) -23.47%, rgb(177, 75, 244) 45.52%, rgb(77, 145, 255) 114.8%) border-box`}
+      // borderColor={!isActive ? color.primary : undefined}
+      radius={9}
+      afterBgColor={color.primary}
+      {...others}
+    />
   );
 }
