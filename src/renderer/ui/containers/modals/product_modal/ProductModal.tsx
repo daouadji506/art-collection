8b70291beb13802fce73dc1b5fe56486402330b6
Daouadji Aymen <daouadji506@gmail.com>
2023-03-25 18:58:34 +0100

    ✅ update ProductModal

--- src/renderer/ui/containers/modals/product_modal/ProductModal.tsx
+++ src/renderer/ui/containers/modals/product_modal/ProductModal.tsx
@@ -4,9 +4,10 @@ import TextPair from '@components/text_pair/TextPair';
 import { color } from '@assets/styles/color';
 import TextButton from '@components/buttons/text_button';
 import Buy from 'toSvg/buy.svg?icon';
+import ModalContainer from '@components/modal_container';
 interface ProductModalProps {
   url: string;
-  alt: string;
+  alt?: string;
   title: string;
   artist: string;
   resolution: string;
@@ -22,92 +23,93 @@ export default function ProductModal({
   resolution,
   price,
 }: ProductModalProps) {
-  //UI reformat to modal container after redefining the theme
   return (
-    <div className="product-modal">
-      <div className="image-item-wrapper">
-        <img src={url} alt={alt} />
-      </div>
-      <BorderSeparator
-        direction="horizontal"
-        color="rgba(255, 255, 255, 0.07)"
-        borderWidth={2}
-      />
-      <div className="footer-container">
-        <div className="footer-info-div">
-          <TextPair
-            gap={2}
-            first={{
-              text: 'Title',
-              fontSize: 12,
-              fontWeight: 500,
-              fontColor: color.textGray,
+    <ModalContainer>
+      <div className="product-modal">
+        <div className="image-item-wrapper">
+          <img src={url} alt={alt} />
+        </div>
+        <BorderSeparator
+          direction="horizontal"
+          color="rgba(255, 255, 255, 0.07)"
+          borderWidth={2}
+        />
+        <div className="footer-container">
+          <div className="footer-info-div">
+            <TextPair
+              gap={2}
+              first={{
+                text: 'Title',
+                fontSize: 12,
+                fontWeight: 500,
+                fontColor: color.textGray,
+              }}
+              second={{
+                text: title,
+                fontSize: 14,
+                fontWeight: 600,
+                fontColor: color.textColor,
+              }}
+            />
+            <TextPair
+              gap={2}
+              first={{
+                text: 'Artist',
+                fontSize: 12,
+                fontWeight: 500,
+                fontColor: color.textGray,
+              }}
+              second={{
+                text: artist,
+                fontSize: 14,
+                fontWeight: 600,
+                fontColor: color.textColor,
+              }}
+            />
+            <TextPair
+              gap={2}
+              first={{
+                text: 'Resolution',
+                fontSize: 12,
+                fontWeight: 500,
+                fontColor: color.textGray,
+              }}
+              second={{
+                text: resolution,
+                fontSize: 14,
+                fontWeight: 600,
+                fontColor: color.textColor,
+              }}
+            />
+            <TextPair
+              gap={2}
+              first={{
+                text: 'Price',
+                fontSize: 12,
+                fontWeight: 500,
+                fontColor: color.textGray,
+              }}
+              second={<span className="colorful-span">{price} $</span>}
+            />
+          </div>
+          <TextButton
+            text="Buy"
+            Icon={{
+              svg: <Buy width={15} height={15} />,
+              iconColor: color.textColor,
+              iconType: 'stroke',
             }}
-            second={{
-              text: title,
-              fontSize: 14,
-              fontWeight: 600,
-              fontColor: color.textColor,
+            fontWeight={600}
+            onPress={onBuy}
+            fontSize={14}
+            radius={9}
+            width={'fit-content'}
+            css={{
+              backgroundImage: color.primary,
             }}
-          />
-          <TextPair
-            gap={2}
-            first={{
-              text: 'Artist',
-              fontSize: 12,
-              fontWeight: 500,
-              fontColor: color.textGray,
-            }}
-            second={{
-              text: artist,
-              fontSize: 14,
-              fontWeight: 600,
-              fontColor: color.textColor,
-            }}
-          />
-          <TextPair
-            gap={2}
-            first={{
-              text: 'Resolution',
-              fontSize: 12,
-              fontWeight: 500,
-              fontColor: color.textGray,
-            }}
-            second={{
-              text: resolution,
-              fontSize: 14,
-              fontWeight: 600,
-              fontColor: color.textColor,
-            }}
-          />
-          <TextPair
-            gap={2}
-            first={{
-              text: 'Price',
-              fontSize: 12,
-              fontWeight: 500,
-              fontColor: color.textGray,
-            }}
-            second={<span className="colorful-span">{price} $</span>}
           />
         </div>
-        <TextButton
-          text="Buy"
-          Icon={{
-            svg: <Buy width={15} height={15} />,
-            iconColor: color.textColor,
-            iconType: 'stroke',
-          }}
-          fontWeight={600}
-          onPress={onBuy}
-          fontSize={14}
-          radius={9}
-          width={'fit-content'}
-          css={{
-            backgroundImage: color.primary,
-          }}
-        />
       </div>
-    </div>
+    </ModalContainer>
   );
 }
