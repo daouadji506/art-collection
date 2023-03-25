21d31d041caa34d9bb837b322bada74535aa6c9e
Daouadji Aymen <daouadji506@gmail.com>
2023-03-25 18:03:14 +0100

    🎨 create gradient LoadingSpinner comp

--- src/renderer/ui/components/loading_spinner/LoadingSpinner.stories.tsx
+++ src/renderer/ui/components/loading_spinner/LoadingSpinner.stories.tsx
@@ -2,7 +2,7 @@ import { ComponentProps } from 'react';
 import type { Story } from '@storybook/react';
 import LoadingSpinner from './LoadingSpinner';
 export default {
-  title: 'LoadingSpinner',
+  title: 'harnan/LoadingSpinner',
   component: LoadingSpinner,
 };
 const Template: Story<ComponentProps<typeof LoadingSpinner>> = (args) => (
--- src/renderer/ui/components/loading_spinner/LoadingSpinner.tsx
+++ src/renderer/ui/components/loading_spinner/LoadingSpinner.tsx
@@ -1,19 +1,17 @@
 import './style/index.scss';
-import Loading from 'toSvg/loading_logo.svg?icon';
 
 interface LoadingSpinnerProps {
   width?: number | string;
   height?: number | string;
 }
 export default function LoadingSpinner({
-  width,
+  width = 50,
   height = 50,
 }: LoadingSpinnerProps) {
   return (
-    <div className="loading-container" css={{ width: width, height: height }}>
-      <div className="loading-spinner">
-        <Loading width={50} height={50} />
-      </div>
-    </div>
+    <div
+      className="loading-container"
+      css={{ width: width, height: height }}
+    ></div>
   );
 }
