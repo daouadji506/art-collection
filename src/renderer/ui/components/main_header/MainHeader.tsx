c36af5158c5a11a0e60802967f6c940316be4030
Daouadji Aymen <daouadji506@gmail.com>
2023-03-27 20:24:58 +0100

    ✅ update MainHeader

--- src/renderer/ui/components/main_header/MainHeader.tsx
+++ src/renderer/ui/components/main_header/MainHeader.tsx
@@ -1,11 +1,14 @@
 import IconicButton from '@components/buttons/iconic_button';
 import StoreBasket from 'toSvg/store_basket.svg';
 import './style/index.scss';
-import color from '@assets/styles/color';
-import { FIT_MODAL, modal } from '@libs/overlay';
+import { color } from '@assets/styles/color';
+import { DEFAULT_MODAL, modal } from '@libs/overlay';
 import CheckoutModal from '@containers/modals/checkout_modal';
+import { useCheckoutStore } from '@stores/checkoutStore';
 interface MainHeaderProps {}
 export default function MainHeader({}: MainHeaderProps) {
+  const cart = useCheckoutStore((state) => state.cart);
+  //TODO favorites button and favorites modal
   return (
     <div className="main-header">
       <div className="title-span-div">
@@ -14,14 +17,18 @@ export default function MainHeader({}: MainHeaderProps) {
       </div>
       <div className="header-controls">
         <div className="count-badge">
-          <span>1</span>
+          <span>{cart.length}</span>
         </div>
         <IconicButton
           Icon={StoreBasket}
           iconSize={20}
           backgroundColor={color.lighten}
           onPress={() => {
-            modal(() => <CheckoutModal />, FIT_MODAL).open();
+            modal(() => <CheckoutModal />, {
+              ...DEFAULT_MODAL,
+              width: '80%',
+              closeBtn: 'outer',
+            }).open();
           }}
         ></IconicButton>
       </div>
